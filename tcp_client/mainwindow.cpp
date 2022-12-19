e10a26c8159e50042b4ded5857d53e45b6b4219f
Rostislav-kslv <rkiselev2000@gmail.com>
2022-12-19 16:44:08 +0200

    tcp client v0.1

--- tcp_client/mainwindow.cpp
+++ tcp_client/mainwindow.cpp
@@ -1,15 +1,95 @@
 #include "mainwindow.hpp"
+
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+
+  connect(&m_socket, &QTcpSocket::connected, this, &MainWindow::connected);
+  connect(&m_socket, &QTcpSocket::disconnected, this,
+          &MainWindow::disconnected);
+  connect(&m_socket, &QTcpSocket::readyRead, this, &MainWindow::readyRead);
+  connect(&m_socket,
+          QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),
+          this, &MainWindow::error);
+  ui->btnConnect->setEnabled(true);
+  ui->btnDisconnect->setEnabled(false);
+  ui->btnSend->setEnabled(false);
+  m_model.setStringList(m_list);
+
+  ui->listView->setModel(&m_model);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::on_btnDisconnect_clicked() {
+  m_socket.close();
+  ui->btnConnect->setEnabled(true);
+  ui->btnDisconnect->setEnabled(false);
+  ui->btnSend->setEnabled(false);
 }
 
+void MainWindow::on_btnConnect_clicked() {
+  if (m_socket.isOpen()) m_socket.close();
+
+  m_name = QInputDialog::getText(
+      this, "Name", "Enter your name: ", QLineEdit::EchoMode::Normal, m_name);
+
+  bool ok;
+  quint16 port = static_cast<quint16>(ui->txtPort->text().toInt(&ok));
+  if (!ok) {
+    QMessageBox::critical(this, "Error", "Please enter a valid port number!");
+    return;
+  }
+  m_socket.connectToHost(ui->txtServer->text(), port);
+  ui->btnConnect->setEnabled(false);
+  ui->btnSend->setEnabled(true);
+  ui->btnDisconnect->setEnabled(true);
+
+  if (!m_socket.waitForConnected(3000)) {
+    on_btnConnect_clicked();
+    QMessageBox::critical(this, "Error", "Could not connect to server!");
+    return;
+  }
+}
+
+void MainWindow::on_btnSend_clicked() {
+  if (!m_socket.isOpen()) return;
+
+  QByteArray data;
+
+  data.append(m_name);
+  data.append(QString(":  "));
+  data.append(ui->txtMessage->text());
+
+  m_socket.write(data);
+  ui->txtMessage->setText(QString());
+}
+
+void MainWindow::connected() {
+  ui->btnConnect->setEnabled(false);
+  ui->btnSend->setEnabled(true);
+  ui->btnDisconnect->setEnabled(true);
+}
+
+void MainWindow::disconnected() {
+  ui->btnConnect->setEnabled(true);
+  ui->btnSend->setEnabled(false);
+  ui->btnDisconnect->setEnabled(false);
+}
+
+void MainWindow::readyRead() {
+  QByteArray data = m_socket.readAll();
+  QString message(data);
+  m_list.append(message);
+  m_model.setStringList(m_list);
+  ui->listView->scrollToBottom();
+}
+
+void MainWindow::error(QAbstractSocket::SocketError socketError) {
+  Q_UNUSED(socketError);
+  m_list.clear();
+  m_model.setStringList(m_list);
+  QMessageBox::critical(this, "Error", m_socket.errorString());
+}
--- tcp_client/mainwindow.hpp
+++ tcp_client/mainwindow.hpp
@@ -1,21 +1,45 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include <QAbstractSocket>
+#include <QInputDialog>
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QStringList>
+#include <QStringListModel>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+ private slots:
+  void on_btnDisconnect_clicked();
+
+  void on_btnConnect_clicked();
+
+  void on_btnSend_clicked();
+
+  void connected();
+  void disconnected();
+  void readyRead();
+  void error(QAbstractSocket::SocketError socketError);
+
+ private:
+  Ui::MainWindow *ui;
+
+  QTcpSocket m_socket;
+  QStringList m_list;
+  QStringListModel m_model;
+  QString m_name;
 };
-#endif // MAINWINDOW_HPP
+#endif  // MAINWINDOW_HPP
